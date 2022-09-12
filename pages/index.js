d188d7fc8b6da4a8137c58d156a52857f60eccac
blockstar-jarod\Jarod Evans <jarod@blockstars.com.au>
2022-09-12 14:20:36 +1000

    Work Paged connected and finished.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Hero from '../components/Hero';
 import Slider from '../components/Slider';
 import { SliderData } from '../components/SliderData';
+import Instagram from '../components/Instagram';
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       </Head>
       <Hero heading='captur photography' message='I capture moments in nature and keep them alive.' />
       <Slider slides={SliderData} />
+      <Instagram />
       </div>
   )
 }
