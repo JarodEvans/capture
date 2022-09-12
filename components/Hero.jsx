6063ac5bcca2c85d8f061c8602a7bb3063ef7f3c
blockstar-jarod\Jarod Evans <jarod@blockstars.com.au>
2022-09-12 10:39:22 +1000

    Fixed text not showing in correct position.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,10 +5,10 @@ const Hero = ({heading, message}) => {
       <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
         {/* overlay */}
         <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]">
-          <div className='p-5 test-white z-[2] ml-[-10rem] my-[-10rem]'>
-            <h2 className='text-5xl font-bold'>{heading}</h2>
-            <p className='py-5 text-xl'>{message}</p>
-            <button className='px-8 py-2 border'>Book</button>
+          <div className='p-5 text-white z-[2] mt-[10rem]'>
+            <h2 className='justify-center text-5xl font-bold'>{heading}</h2>
+            <p className='justify-center py-5 text-xl'>{message}</p>
+            <button className='justify-center px-8 py-2 border'>Book</button>
           </div>
         </div>
       </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,49 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        // change state from true to false or vice versa
+        setNav(!nav)
+    };
+
     return (
       <div className='fixed left-0 top-0 w-full z-10 eas-in duration-300'>
         <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
           <Link href="/">
-            <h1>Captur</h1>
+            <h1 className='font-bold text-4xl'>Captur</h1>
           </Link>
-          <ul>
-            <li>
+          <ul className='hidden sm:flex className='>
+            <li className='p-4'>
               <Link href="/">Home</Link>
             </li>
-            <li>
+            <li className='p-4'>
               <Link href="/#gallery">Gallery</Link>
             </li>
-            <li>
+            <li className='p-4'>
               <Link href="/work">Work</Link>
             </li>
-            <li>
+            <li className='p-4'>
               <Link href="/contact">Contact</Link>
             </li>
           </ul>
+
           {/* Mobile button */}
-          <div>
-            <AiOutlineClose size={20} />
+          <div className='block sm:hidden z-10'>
+            {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
           </div>
           {/* Mobile Menu */}
-          <di>
+          <div className={nav ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300' : 
+          'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'}>
             <ul>
-              <li>
+              <li className='p-4 text-4xl hover:text-gray-500'>
                 <Link href="/">Home</Link>
               </li>
-              <li>
+              <li className='p-4 text-4xl hover:text-gray-500'>
                 <Link href="/#gallery">Gallery</Link>
               </li>
-              <li>
+              <li className='p-4 text-4xl hover:text-gray-500'>
                 <Link href="/work">Work</Link>
               </li>
-              <li>
+              <li className='p-4 text-4xl hover:text-gray-500'>
                 <Link href="/contact">Contact</Link>
               </li>
             </ul>
-          </di>
+          </div>
         </div>
       </div>
     );
