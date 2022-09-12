69758802034c4ace3d587ea1d8933859a4bad916
blockstar-jarod\Jarod Evans <jarod@blockstars.com.au>
2022-09-12 14:32:47 +1000

    Completed.

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Contact from '../components/Contact';
 
 const contact = () => {
     return ( 
         <div>
-            Contact
+            <Hero heading='Contacts' message='Submit the form below for more work and qotes.'/>
+            <Contact />
         </div>
      );
 }
